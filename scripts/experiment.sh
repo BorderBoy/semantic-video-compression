1036d726b6883a76ad5e58f9c82b57776a0a1f88
BorderBoy <jonas.goos@tum.de>
2024-02-15 13:54:36 +0100

    Add support for extracting resolution and fps from filename

--- scripts/experiment.sh
+++ scripts/experiment.sh
@@ -8,6 +8,8 @@ ROI_MODES=("none" "face" "fes")
 FPS=30
 RESOLUTION="1280x720"
 REPEATS=3 # number of times each encoding is done
+RES_PATTERN='encoded\ ([0-9]+)\ frames,\ ([0-9.]+)\ fps,\ ([0-9.]+)\ kb/s'
+FILE_PATTERN='.+_([0-9x]+)_([0-9]+)'
 
 # clear results file
 echo "name,encoding time,encoded frames,fps,bitrate" > $RES_PATH/results.csv
@@ -25,7 +27,16 @@ for mode in "${ROI_MODES[@]}"; do
     for br in "${BITRATES[@]}"; do
         for file in $RECS_PATH/*; do
             for (( i=0; i<$REPEATS; i++ )); do   
-                out_file=$(basename ${file} .yuv)_"$br"_"$mode"
+                base_name=$(basename $file .yuv)
+                
+                if [[ $base_name =~ $FILE_PATTERN ]]; then
+                    RESOLUTION=${BASH_REMATCH[1]}
+                    FPS=${BASH_REMATCH[2]}
+                else
+                    echo "Failed to fps and resolution from filename. Using default values."
+                fi
+
+                out_file="$base_name"_"$br"_"$mode"
                 params="--quiet --no-progress --profile baseline -B $br --fps $FPS --input-res $RESOLUTION -o "$RES_PATH"/$out_file.h264 $file"
                 if [ "$mode" != "none" ]; then
                     params="--aq-mode 4 $params"
@@ -38,13 +49,12 @@ for mode in "${ROI_MODES[@]}"; do
                 elapsed=$(echo "$end - $start" | bc)
 
                 # extract frames encoded, fps and bitrate from output
-                pattern='encoded\ ([0-9]+)\ frames,\ ([0-9.]+)\ fps,\ ([0-9.]+)\ kb/s'
-                if [[ $output =~ $pattern ]]; then
+                if [[ $output =~ $RES_PATTERN ]]; then
                     frames=${BASH_REMATCH[1]}
                     fps=${BASH_REMATCH[2]}
                     bitrate=${BASH_REMATCH[3]}
                 else
-                    echo "Failed to extract values from the input string."
+                    echo "Failed to extract values from x264 output string."
                 fi
 
                 echo $out_file,$elapsed,$frames,$fps,$bitrate >> $RES_PATH/results.csv
